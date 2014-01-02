7f862ef5dd86aac8d86efe6df5f7a486993987fd
Dylan Foster <turtlecannon@gmail.com>
2014-01-02 07:18:09 -0700

    Basins of attraction working.

---
+++ hw1/p2a.m
@@ -0,0 +1,4 @@
+% These fixed points were solved for by hand for the given parameters
+% calc_basin(0, 0, [0; 0]); % No damping means that all states not
+% starting at [nPI, 0] will end up in limit cycles
+calc_basin(0, 0.5, [0; 0]);
